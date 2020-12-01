904c09365a41a888fdd04068f4ed6852917401af
= <=>
2020-12-01 10:57:49 -0900

    Merge with main. Started update on business pages.

--- app/src/components/ProfileTemplateVendor.vue
+++ app/src/components/ProfileTemplateVendor.vue
@@ -10,7 +10,7 @@
                   <div class="d-flex flex-column align-items-center text-center">
                     <img src="https://cdn.searchenginejournal.com/wp-content/uploads/2018/04/businesses-need-seo-760x400.png" alt="Admin" class="rounded-circle" width="150">
                     <div class="mt-3">
-                      <h4>Franky Fast-Hands</h4>
+                      <h4>Kitton Mittons</h4>
                       <p class="text-secondary mb-1">Black Market Kitten Dealership</p>
                       <p class="text-muted font-size-sm">Back Alley, NY</p>
                     </div>
@@ -19,10 +19,6 @@
               </div>
               <div class="card mt-3">
                 <ul class="list-group list-group-flush">
-                  <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                    <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-globe mr-2 icon-inline"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>Website</h6>
-                    <span class="text-secondary">https://kittonmitton.com</span>
-                  </li>
                   <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
                     <h6 class="mb-0"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>Twitter</h6>
                     <span class="text-secondary">@bootdey</span>
@@ -43,10 +39,10 @@
                 <div class="card-body">
                   <div class="row">
                     <div class="col-sm-3">
-                      <h6 class="mb-0">Full Name</h6>
+                      <h6 class="mb-0">Website</h6>
                     </div>
                     <div class="col-sm-9 text-secondary">
-                      Franklin Roosevelt of the Quick Wit
+                      ftp://kittonmittons.com
                     </div>
                   </div>
                   <hr>
